e6f86b0744a688dfdf2c0a487916d80c930dff3f
giovanniKleinCampigoto <giovannikleincampigoto@gmail.com>
2020-08-06 17:15:36 -0300

    [*] Finish frontend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -2,36 +2,53 @@ import React from "react";
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg";
 
 import "./styles.css";
+import api from "../../services/api";
 
-const TeacherItem = () => {
+export interface Teacher {
+  avatar: string;
+  bio: string;
+  cost: number;
+  id: number;
+  name: string;
+  subject: string;
+  whatsapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post("connections", {
+      user_id: teacher.id,
+    });
+  }
+
   return (
     <article className='teacher-item'>
       <header>
-        <img
-          src='https://avatars2.githubusercontent.com/u/23245646?s=460&u=65d93074f88eea831a548417604c17c56fc85ed7&v=4'
-          alt='Giovanni Klein Campigoto'
-        />
+        <img src={teacher.avatar} alt={teacher.name} />
         <div>
-          <strong>Giovanni Klein Campigoto</strong>
-          <span>Química</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
-      <p>
-        Entusiasta das melhores tecnologias de química avançada.
-        <br />
-        <br />
-        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através das
-        experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões
-      </p>
+      <p>{teacher.bio}</p>
       <footer>
         <p>
-          Preço/hora <strong>R$ 80,00</strong>
+          Preço/hora <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type='button'>
+        <a
+          onClick={createNewConnection}
+          type='button'
+          target='_blank'
+          href={` https://wa.me/${teacher.whatsapp}`}
+        >
           <img src={whatsappIcon} alt='Whatsapp' />
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
